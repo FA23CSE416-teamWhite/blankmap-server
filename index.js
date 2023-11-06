c66131347026c2376d96199cab9e8618485524b5
Apricot0 <61392940+Apricot0@users.noreply.github.com>
2023-11-06 03:06:35 -0500

    untract node modules and updated server and APP, now it works locally

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-// Import required packages
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables
-
-// Create an Express application
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path')
+const PORT = process.env.PORT || 8000;
 const app = express();
+const User = require('./models/user');
+require('./db/database');
 
-// Define the port
-const PORT = process.env.PORT || 3000;
-
-// Connect to MongoDB Atlas
-mongoose.connect(process.env.DB_CONNECT, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Connected to MongoDB Atlas');
-})
-.catch((error) => {
-    console.error('Error connecting to MongoDB Atlas:', error);
-});
-
-// Start the server
+// app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  }));
+app.use(express.json());
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
+app.post('/api/users', async (req, res) => {
+    const { name, email } = req.body;
+    console.log(req.body);
+    const newUser = new User({
+        name: name,
+        email: email
+    });
+
+    newUser.save()
+        .then(() => {
+            console.log("Account created successfully");
+            res.json({
+                message: "Created account successfully"
+            });
+        })
+        .catch(err => {
+            console.error("Error creating account:", err);
+            res.status(400).json({
+                error: err,
+                message: "Error creating account"
+            });
+        });
+});
+
--- tests/api.test.js
+++ tests/api.test.js
@@ -8,3 +8,17 @@ describe('API Tests', () => {
     expect(res.text).toEqual('Hello World!');
   });
 });
+describe('Basic Server Tests', () => {
+  it('should return a 200 status code for GET /', (done) => {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
+describe('Basic Server Tests', () => {
+  it('should return a 404 status code for invalid routes', (done) => {
+    request(app)
+      .get('/invalid-route')
+      .expect(404, done);
+  });
+});
