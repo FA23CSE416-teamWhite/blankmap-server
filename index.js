db96f2b18a82bff0106ab635134a7ef844e9544f
JunHaoXia <99214774+JunHaoXia@users.noreply.github.com>
2023-11-11 15:50:08 -0500

    --forceExit for jest

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('/api/users', (req, res) => {
 })
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World CSE416!');
+    res.send('Hello World!');
   });
 
 app.put('/put', (req, res) => {
