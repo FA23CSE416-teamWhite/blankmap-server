65d2920b728afba9f7aa085fc5c4837a08061777
Sky Lei <ssskylei@gmail.com>
2023-11-23 18:00:14 -0500

    Update auth-router.js

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const AuthController = require('../controllers/auth-controller')
 
-router.post('/registers', AuthController.registerUser)
-router.put('/logins', AuthController.loginUser)
+router.post('/register', AuthController.registerUser)
+router.post('/login', AuthController.loginUser)
 router.get('/logout', AuthController.logoutUser)
 router.get('/loggedIn', AuthController.getLoggedIn)
 
