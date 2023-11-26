8a315e33409f9a73d68e3f60b198d2ba3ca5dd56
someguy2 <nathanchan4121@gmail.com>
2023-11-26 11:06:55 -0500

    fixed updateuser again

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -5,10 +5,10 @@ const AuthController = require('../controllers/auth-controller')
 router.post('/register', AuthController.registerUser)
 router.post('/login', AuthController.loginUser)
 router.post('/update', AuthController.updateUser)
+router.post('/reset/:email', AuthController.updateUser)
 router.get('/logout', AuthController.logoutUser)
 router.get('/loggedIn', AuthController.getLoggedIn)
 router.get('/question/:email', AuthController.getQuestion)
-router.get('/reset/:email', AuthController.updateUser)
 router.get('/update', AuthController.updateUser)
 
 module.exports = router
