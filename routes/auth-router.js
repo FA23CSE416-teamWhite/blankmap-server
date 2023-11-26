6ae9b745f7112fd176a9189d268cd8b9d5037f6e
someguy2 <nathanchan4121@gmail.com>
2023-11-26 02:34:24 -0500

    added update to router

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -4,6 +4,7 @@ const AuthController = require('../controllers/auth-controller')
 
 router.post('/register', AuthController.registerUser)
 router.post('/login', AuthController.loginUser)
+router.post('/update', AuthController.updateUser)
 router.get('/logout', AuthController.logoutUser)
 router.get('/loggedIn', AuthController.getLoggedIn)
 router.get('/question', AuthController.getQuestion)
